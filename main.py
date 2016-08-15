d1c9bd492fc803bb1993ac8ac49c0326deecee3a
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2016-08-15 20:03:40 +0300

    fix bugs

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def handle_command(message):
     bot.send_message(config.chat_id, 'Скачиваю книгу с внешнего ресурса...')
     download_link = LitRu.get_book(message.text)
     file_name = message.text.split('_')[1]
-    download_book(download_link, file_name)
+    download_book(download_link, file_name) 
 
 
 @bot.message_handler(content_types=['text'])
